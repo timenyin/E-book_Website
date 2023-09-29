f5eb101a5791be374fdeceb4f02d4bf8c112402c
Harmony <47876447+timenyin@users.noreply.github.com>
2023-09-29 16:55:14 +0100

    fri- spt-2023 29

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
 // Add custom JavaScript here
+function userScroll() {
+    const navbar = document.querySelector('.navbar');
+
+    window.addEventListener('scroll', () => {
+        if(window.scrollY > 50) {
+            navbar.classList.add('bg-dark');
+            navbar.classList.add('navbar-sticky')
+        } else {
+            navbar.classList.remove('bg-dark');
+            navbar.classList.add('navbar-sticky')
+        }
+    });
+}
+
+document.addEventListener('DOMContentLoaded', userScroll);
+
+
+// function colornow() {
+//     const color = document.querySelector('.container');
+
+//     window.addEventListener('colorY', () => {
+//        if(window.scrollTo) {
+//         navbar.classList.add('bg-dark');
+//        }else{
+//         navbar.classList.remove(bg-dark);
+//        }
+//     });
+// }
